91aeedacf52d1b5d16ff23491576d4786256aea6
wxr <XingrWang@163.com>
2020-12-23 09:43:22 +0800

    update

--- generate.py
+++ generate.py
@@ -9,7 +9,7 @@ from stylegan2 import utils
 
 
 def synthesis(G_file, latent_file):
-    device = torch.device(0)
+    device = torch.device('cpu')
     G = stylegan2.models.load(G_file).G_synthesis 
     latent = np.load(latent_file)
     G.to(device)
@@ -23,6 +23,11 @@ def synthesis(G_file, latent_file):
 if __name__ == '__main__':
 
     # out = synthesis('checkpoints/stylegan2_512x512_with_pretrain_new_2/10000_2020-12-22_03-42-54/Gs.pth', 'projects/latent/image0000-step1000.npy')
-    out = synthesis('G_out.pth', 'projects/latent/image0000-step1000.npy')
+    out = synthesis('G_out.pth', 'projects/latent/image0000-step0011.npy')
+    # out = synthesis('checkpoints/stylegan2_512x512_with_pretrain/pretrain/Gs.pth', '/home/wxr/stylegan2_pytorch_backup/projects/latent/zj.npy')
     # G = stylegan2.models.load('checkpoints/stylegan2_512x512_with_pretrain/pretrain/Gs.pth').G_synthesis # 
     out.save('out.png')
+    # for s in ['0041','0081','0121','0161','0200']:
+    #     out = synthesis('G_out.pth', 'projects/latent/image0000-step%s.npy'%(s))
+    #     out.save('out%s.png'%(s))
+
---
+++ latent.sh
@@ -0,0 +1,2 @@
+python run_projector.py project_real_images --network=checkpoints/stylegan2_512x512_with_pretrain/pretrain/Gs.pth --data_dir=./projects/real/ --output=./projects/latent/ --num_steps=50 --gpu=1
+# python run_projector.py project_real_images --network=G_out_6.pth --data_dir=./projects/real/ --output=./projects/latent/ --num_steps=500
